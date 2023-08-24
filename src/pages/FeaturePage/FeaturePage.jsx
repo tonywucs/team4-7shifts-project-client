acb165354377ef4b962b530543987e62feffd9ff
Tony Wu <cs.tony.wu@gmail.com>
2023-08-23 21:22:35 -0400

    Replace Vite boilerplate with create-react-app to reduce unknowns

---
+++ src/pages/FeaturePage/FeaturePage.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const FeaturePage = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+};
+
+export default FeaturePage;
