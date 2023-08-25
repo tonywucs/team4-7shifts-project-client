0b0b8a3f80f25420b350e48c6ba7b1453a25d2eb
Sarah Reeve <sarahereeve2@gmail.com>
2023-08-24 22:39:57 -0400

    Add 7shifts component library install, add background image to app.js

---
+++ src/components/button/button.js
@@ -0,0 +1,16 @@
+import "@7shifts/sous-chef/dist/index.css";
+
+function button({ text }) {
+
+    return (
+
+
+        <div className="button__publish">
+            {text}
+        </div>
+    )
+
+}
+
+export default button;
+
--- src/pages/FeaturePage/FeaturePage.jsx
+++ src/pages/FeaturePage/FeaturePage.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "../../App.scss"
+import backgroundImage from "../../assets/images/schedule2.png"
 
 const FeaturePage = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            {/* <div className="background__image"></div> */}
+            <img className= "background__image" src={backgroundImage}/>
         </div>
     );
 };
