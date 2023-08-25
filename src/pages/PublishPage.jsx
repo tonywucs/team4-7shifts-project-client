79cf84fa1d8fb5514df19a9eb77b98315885860f
Sarah Reeve <sarahereeve2@gmail.com>
2023-08-25 13:06:31 -0400

    Adds after background image and indicator icon

---
+++ src/pages/PublishPage.jsx
@@ -0,0 +1,11 @@
+import afterImage from "../assets/images/screenshotAfter.png"
+
+const PublishPage = () => {
+    return (
+        <>
+             <img className="background__image" src={afterImage} />
+        </>
+    );
+};
+
+export default PublishPage;
